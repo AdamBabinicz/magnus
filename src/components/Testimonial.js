4ecc4a043eac3aed18a8a6ed339e14a82898a87d
babinicz <babinicz@onet.pl>
2021-04-22 15:59:48 +0200

    zmiana top w button

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -30,7 +30,7 @@ const Testimonial = () => {
           />
         </div>
         <div className="relative lg:px-32 text-center lg:text-left mt-48 lg:mt-24">
-          <button className="absolute left-36 top-72 lg:top-48 outline-none bg-black bg-opacity-60 text-white font-bold uppercase py-4 px-10 transition-all rounded-full hover:bg-black bg-opacity-40">
+          <button className="absolute left-36 top-24 lg:top-48 outline-none bg-black bg-opacity-60 text-white font-bold uppercase py-4 px-10 transition-all rounded-full hover:bg-black bg-opacity-40">
             Czytaj więcej
           </button>
         </div>
