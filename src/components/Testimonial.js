623908d9f0bf4e4a48b7a451ca895049a32dc299
babinicz <babinicz@onet.pl>
2021-04-22 17:47:12 +0200

    zmiana h zdjęcia

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -84,7 +84,7 @@ const Testimonial = () => {
             </p>
             <br />
             <div className="flex justify-center">
-              <img src={marekImg} alt="..." />
+              <img src={marekImg} alt="..." className="h-auto" />
             </div>
             <br />
             <p className="lg:px-10 sm:px-2 lg:text-left sm:text-justify">
